1730725bc6b11878fb4b243806e8bcdf197c320d
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-08-27 11:48:19 +0200

    feat: add enabled settings

--- src/VirtoCommerce.XRecommend.Core/ModuleConstants.cs
+++ src/VirtoCommerce.XRecommend.Core/ModuleConstants.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using VirtoCommerce.Platform.Core.Settings;
+
 namespace VirtoCommerce.XRecommend.Core;
 
 public static class ModuleConstants
@@ -20,4 +23,45 @@ public static class ModuleConstants
             };
         }
     }
+
+    public static class Settings
+    {
+        public static class General
+        {
+            public static SettingDescriptor RecommendationsEnabled { get; } = new()
+            {
+                Name = "XRecommend.RecommendationsEnabled",
+                GroupName = "Recommendations|General",
+                ValueType = SettingValueType.Boolean,
+                IsPublic = true,
+                DefaultValue = true,
+            };
+
+
+            public static IEnumerable<SettingDescriptor> AllGeneralSettings
+            {
+                get
+                {
+                    yield return RecommendationsEnabled;
+                }
+            }
+        }
+
+        public static IEnumerable<SettingDescriptor> AllSettings
+        {
+            get
+            {
+                return General.AllGeneralSettings;
+            }
+        }
+
+        public static IEnumerable<SettingDescriptor> StoreLevelSettings
+        {
+            get
+            {
+                yield return General.RecommendationsEnabled;
+            }
+        }
+    }
+
 }
--- src/VirtoCommerce.XRecommend.Web/Module.cs
+++ src/VirtoCommerce.XRecommend.Web/Module.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using VirtoCommerce.Platform.Core.Modularity;
 using VirtoCommerce.Platform.Core.Security;
+using VirtoCommerce.Platform.Core.Settings;
+using VirtoCommerce.StoreModule.Core.Model;
 using VirtoCommerce.Xapi.Core.Extensions;
 using VirtoCommerce.Xapi.Core.Infrastructure;
 using VirtoCommerce.XRecommend.Core;
@@ -32,6 +34,11 @@ public class Module : IModule, IHasConfiguration
         // Register permissions
         var permissionsRegistrar = serviceProvider.GetRequiredService<IPermissionsRegistrar>();
         permissionsRegistrar.RegisterPermissions(ModuleInfo.Id, "XRecommend", ModuleConstants.Security.Permissions.AllPermissions);
+
+        // Register settings
+        var settingsRegistrar = serviceProvider.GetRequiredService<ISettingsRegistrar>();
+        settingsRegistrar.RegisterSettings(ModuleConstants.Settings.AllSettings, ModuleInfo.Id);
+        settingsRegistrar.RegisterSettingsForType(ModuleConstants.Settings.StoreLevelSettings, nameof(Store));
     }
 
     public void Uninstall()
