167964b1f865a14951b40c0a7f6cfffa41f7e477
Alexey Shibanov <83034617+alexeyshibanov@users.noreply.github.com>
2024-09-13 14:15:46 +0300

    feat: Make graph types extendable (#6)

--- src/VirtoCommerce.XRecommend.Core/Schemas/GetRecentlyBrowsedResponseType.cs
+++ src/VirtoCommerce.XRecommend.Core/Schemas/GetRecentlyBrowsedResponseType.cs
@@ -1,13 +1,14 @@
 using GraphQL.Types;
+using VirtoCommerce.Xapi.Core.Schemas;
 using VirtoCommerce.XCatalog.Core.Schemas;
 using VirtoCommerce.XRecommend.Core.Models;
 
 namespace VirtoCommerce.XRecommend.Core.Schemas;
 
-public class GetRecentlyBrowsedResponseType : ObjectGraphType<GetRecentlyBrowsedResult>
+public class GetRecentlyBrowsedResponseType : ExtendableGraphType<GetRecentlyBrowsedResult>
 {
     public GetRecentlyBrowsedResponseType()
     {
-        Field<ListGraphType<ProductType>>("products", resolve: context => context.Source.Products);
+        ExtendableField<ListGraphType<ProductType>>("products", resolve: context => context.Source.Products);
     }
 }
--- src/VirtoCommerce.XRecommend.Core/Schemas/GetRecommendationsResponseType.cs
+++ src/VirtoCommerce.XRecommend.Core/Schemas/GetRecommendationsResponseType.cs
@@ -1,13 +1,14 @@
 using GraphQL.Types;
+using VirtoCommerce.Xapi.Core.Schemas;
 using VirtoCommerce.XCatalog.Core.Schemas;
 using VirtoCommerce.XRecommend.Core.Models;
 
 namespace VirtoCommerce.XRecommend.Core.Schemas;
 
-public class GetRecommendationsResponseType : ObjectGraphType<GetRecommendationsResult>
+public class GetRecommendationsResponseType : ExtendableGraphType<GetRecommendationsResult>
 {
     public GetRecommendationsResponseType()
     {
-        Field<ListGraphType<ProductType>>("products", resolve: context => context.Source.Products);
+        ExtendableField<ListGraphType<ProductType>>("products", resolve: context => context.Source.Products);
     }
 }
