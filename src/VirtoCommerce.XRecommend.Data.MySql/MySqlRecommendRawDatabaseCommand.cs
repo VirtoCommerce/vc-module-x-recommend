6591d3ecd2d38fa8467d9376c846ee55ba36bb13
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2024-09-17 15:09:18 +0200

    feat: add mysql and postgre queries

--- src/VirtoCommerce.XRecommend.Data.MySql/MySqlRecommendRawDatabaseCommand.cs
+++ src/VirtoCommerce.XRecommend.Data.MySql/MySqlRecommendRawDatabaseCommand.cs
@@ -1,3 +1,5 @@
+using MySqlConnector;
+using VirtoCommerce.Platform.Data.Extensions;
 using VirtoCommerce.XRecommend.Core.Models;
 using VirtoCommerce.XRecommend.Data.Repositories;
 
@@ -5,8 +7,33 @@ namespace VirtoCommerce.XRecommend.Data.MySql;
 
 public class MySqlRecommendRawDatabaseCommand : IRecommendRawDatabaseCommand
 {
-    public Task<IList<string>> GetBoughtTogetherProductIdsAsync(XRecommendDbContext context, GetRecommendationsCriteria criteria)
+    public async Task<IList<string>> GetBoughtTogetherProductIdsAsync(XRecommendDbContext context, GetRecommendationsCriteria criteria)
     {
-        throw new NotImplementedException();
+        var commandTemplate = @"
+            SELECT ProductId 
+            FROM HistoricalEvents he
+            INNER JOIN (
+                SELECT SessionId 
+                FROM HistoricalEvents
+                WHERE StoreId = @storeId 
+                  AND ProductId = @productId 
+                  AND UserId <> @userId 
+                  AND EventType IN ('addToCart', 'placeOrder')
+            ) s ON s.SessionId = he.SessionId
+            WHERE he.ProductId <> @productId
+            GROUP BY ProductId
+            ORDER BY COUNT(ProductId) DESC
+            LIMIT @limit;";
+
+        var parameters = new object[]
+        {
+            new MySqlParameter("@storeId", criteria.StoreId),
+            new MySqlParameter("@productId", criteria.ProductId),
+            new MySqlParameter("@userId", criteria.UserId),
+            new MySqlParameter("@limit", criteria.MaxRecommendations),
+        };
+
+        var result = await context.ExecuteArrayAsync<string>(commandTemplate, parameters);
+        return result;
     }
 }
--- src/VirtoCommerce.XRecommend.Data.PostgreSql/PostgreSqlRecommendRawDatabaseCommand.cs
+++ src/VirtoCommerce.XRecommend.Data.PostgreSql/PostgreSqlRecommendRawDatabaseCommand.cs
@@ -1,3 +1,5 @@
+using Npgsql;
+using VirtoCommerce.Platform.Data.Extensions;
 using VirtoCommerce.XRecommend.Core.Models;
 using VirtoCommerce.XRecommend.Data.Repositories;
 
@@ -5,8 +7,34 @@ namespace VirtoCommerce.XRecommend.Data.PostgreSql;
 
 public class PostgreSqlRecommendRawDatabaseCommand : IRecommendRawDatabaseCommand
 {
-    public Task<IList<string>> GetBoughtTogetherProductIdsAsync(XRecommendDbContext context, GetRecommendationsCriteria criteria)
+    public async Task<IList<string>> GetBoughtTogetherProductIdsAsync(XRecommendDbContext context, GetRecommendationsCriteria criteria)
     {
-        throw new NotImplementedException();
+        var commandTemplate = @"
+            WITH SessionIDs AS
+            (
+	            SELECT ""SessionId"" FROM ""HistoricalEvents""
+	            WHERE
+	            ""StoreId"" = @storeId AND
+	            ""ProductId"" = @productId AND
+	            ""UserId"" <> @userId AND
+	            ""EventType"" IN ('addToCart','placeOrder')
+            )
+            SELECT ""ProductId"" FROM ""HistoricalEvents"" he
+            INNER JOIN SessionIDs s ON s.""SessionId"" = he.""SessionId""
+            WHERE he.""ProductId"" <> @productId
+            GROUP BY ""ProductId""
+            ORDER BY COUNT(""ProductId"") DESC
+            LIMIT @limit";
+
+        var parameters = new object[]
+        {
+            new NpgsqlParameter("@storeId", criteria.StoreId),
+            new NpgsqlParameter("@productId", criteria.ProductId),
+            new NpgsqlParameter("@userId", criteria.UserId),
+            new NpgsqlParameter("@limit", criteria.MaxRecommendations),
+        };
+
+        var result = await context.ExecuteArrayAsync<string>(commandTemplate, parameters);
+        return result;
     }
 }
