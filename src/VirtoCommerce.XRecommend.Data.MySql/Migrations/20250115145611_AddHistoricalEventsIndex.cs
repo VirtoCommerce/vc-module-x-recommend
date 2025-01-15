60c9c2efc6ac787f2babfc064fb832846fcf0af1
Konstantin Savosteev <Konstantin.Savosteev@virtoway.com>
2025-01-15 17:01:31 +0200

    chore: create Historical Events index

---
+++ src/VirtoCommerce.XRecommend.Data.MySql/Migrations/20250115145611_AddHistoricalEventsIndex.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VirtoCommerce.XRecommend.Data.MySql.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHistoricalEventsIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoricalEvents_EventType_ProductId_StoreId_UserId",
+                table: "HistoricalEvents",
+                columns: ["EventType", "ProductId", "StoreId", "UserId"]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_HistoricalEvents_EventType_ProductId_StoreId_UserId", table: "HistoricalEvents");
+        }
+    }
+}
